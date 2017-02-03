6833580a7a7118931091e35e3c102c4a288d7ae0
Oleksandr Veshcherovych <s.veshcherovych@gmail.com>
2017-02-03 20:34:51 +0200

    add osmdroid integration

--- app/src/main/java/com/alexveshcher/whatsaround/MainActivity.java
+++ app/src/main/java/com/alexveshcher/whatsaround/MainActivity.java
@@ -2,22 +2,33 @@ package com.alexveshcher.whatsaround;
 
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.common.api.GoogleApiClient.*;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+//https://github.com/osmdroid/osmdroid/wiki/How-to-use-the-osmdroid-library
 
 public class MainActivity extends AppCompatActivity implements
         ConnectionCallbacks, OnConnectionFailedListener {
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
+    private MapView map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +43,27 @@ public class MainActivity extends AppCompatActivity implements
                     .addApi(LocationServices.API)
                     .build();
         }
+
+        //important! set your user agent to prevent getting banned from the osm servers
+//        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        setContentView(R.layout.activity_main);
+
+        map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+
+
+    }
+
+    public void onResume(){
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
     }
 
     protected void onStart() {
@@ -65,8 +97,15 @@ public class MainActivity extends AppCompatActivity implements
                 mGoogleApiClient);
         if (mLastLocation != null) {
             Log.d("mLastLocation: ","true");
-            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
-            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+            double latitude = mLastLocation.getLatitude();
+            double longitude = mLastLocation.getLongitude();
+            mLatitudeText.setText(String.valueOf(latitude));
+            mLongitudeText.setText(String.valueOf(longitude));
+
+            IMapController mapController = map.getController();
+            mapController.setZoom(14);
+            GeoPoint startPoint = new GeoPoint(latitude, longitude);
+            mapController.setCenter(startPoint);
         }
     }
 
